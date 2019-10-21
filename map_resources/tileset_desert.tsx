d2c9ec72368254b38c44a852b29b3dd5db68c48f
PMA <pitsch@gmail.com>
2019-10-21 21:35:44 +0200

    Resources

---
+++ map_resources/tileset_desert.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tileset_desert" tilewidth="32" tileheight="32" tilecount="2" columns="2">
+ <image source="../src/main/resources/tileset_desert.png" width="64" height="32"/>
+</tileset>
