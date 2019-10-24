98969be2e04bb7e29e69c4ca1133b2fdf201f32e
PMA <pitsch@gmail.com>
2019-10-24 22:29:26 +0200

    Cannon Tip rotation finalized Abstraction layer for towers

--- src/main/java/ch/pma/liti/tankz/objects/Tank.java
+++ src/main/java/ch/pma/liti/tankz/objects/Tank.java
@@ -7,7 +7,6 @@ import de.gurkenlabs.litiengine.annotation.CollisionInfo;
 import de.gurkenlabs.litiengine.annotation.EntityInfo;
 import de.gurkenlabs.litiengine.annotation.MovementInfo;
 import de.gurkenlabs.litiengine.entities.Creature;
-import de.gurkenlabs.litiengine.graphics.RenderType;
 
 import java.awt.*;
 
@@ -16,21 +15,11 @@ import java.awt.*;
 @CollisionInfo(collisionBoxWidth = 81, collisionBoxHeight = 40, collision = true)
 public class Tank extends Creature implements IUpdateable {
 
-    private static Tank instance;
-
-    public static Tank instance(ITankMovementListener tankMovementListener) {
-        if (instance == null) {
-            instance = new Tank(tankMovementListener);
-            instance.setRenderType(RenderType.SURFACE); //set render type for the tank to rendered behind the tower
-        }
-        return instance;
-    }
-
-    private Tank(ITankMovementListener tankMovementListener) {
+    public Tank(ITankMovementListener tankMovementListener) {
         //IMPORTANT: Sprites must follow the naming convention when being imported into the utiLITI
         super("tank1");
         // setup movement controller
-        this.addController(new TankMovementController<Tank>(this, new Point((int)this.getWidth() / 2, (int)this.getHeight() / 2), tankMovementListener));
+        this.addController(new TankMovementController<>(this, new Point((int)this.getWidth() / 2, (int)this.getHeight() / 2), tankMovementListener));
     }
 
     @Override
